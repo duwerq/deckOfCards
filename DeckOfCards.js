89839572a8ebe8b3052fc5890445907601a29d9e
Matthew <mattcoldbeans@gmail.com>
2017-04-24 08:30:36 -0500

    Create DeckOfCards.js

--- DeckOfCards.js
+++ DeckOfCards.js
@@ -1,71 +1,69 @@
-function Card(suit, rank) {
-  this.suit = suit;
-  this.rank = this.rank;
-}
+class Rectangle {
+  var ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  var suits = ["♠","♥", "♦", "♣"];
 
-var ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-var suits = ["♠","♥", "♦", "♣"];
-
-function createDeck() {
-  var deck = []
-  suits.map(function(suit) {
-    ranks.map(function(rank,i) {
-      deck.push(suit + rank)
+  function createDeck() {
+    var deck = []
+    suits.map(function(suit) {
+      ranks.map(function(rank,i) {
+        deck.push(suit + rank)
+      })
     })
-  })
-  return deck
-}
-
-var fullDeck = createDeck()
-var shuffledDeck = fullDeck
-
-function shuffleCards(deck) {
-  shuffDeck = []
-  temp = []
-  function random(){
-    return Math.floor(Math.random() * 52)
-   }
-  deck.map(function(card) {
-    var p = random();
-    if (temp.indexOf(p) < 0) {
-      shuffDeck.push(deck[p])
-      temp.push(p)
-    } else {
-      while(temp.indexOf(p) > -1) {
-        p = random()
-        if(temp.indexOf(p) < 0) {
-          shuffDeck.push(deck[p])
-          break;
-        } 
-      }
-    }
-  })
-  return shuffDeck
-}
-
-function dealCards(number, faceDown, position){
-  var dealtCards = []
-  var selectedCards = shuffledDeck.splice(0,number)
-  if (number > shuffledDeck.length) {
-    var notEnoughCards = shuffledDeck.splice(0,number)
-    notEnoughCards.map((card) => {
-      dealtCards.push({card: card, faceDown: faceDown, position: position})
-    })
-    dealtCards.push({card: "No More Cards"})
-  } else {
-  selectedCards.map((card) => {
-  dealtCards.push({card: card, faceDown: faceDown, position: position})
-  })
+    return deck
   }
-  return dealtCards
+
+  var fullDeck = createDeck()
+  var shuffledDeck = fullDeck
+
+  function shuffleCards(deck) {
+    shuffDeck = []
+    temp = []
+    function random(){
+      return Math.floor(Math.random() * 52)
+     }
+    deck.map(function(card) {
+      var p = random();
+      if (temp.indexOf(p) < 0) {
+        shuffDeck.push(deck[p])
+        temp.push(p)
+      } else {
+        while(temp.indexOf(p) > -1) {
+          p = random()
+          if(temp.indexOf(p) < 0) {
+            shuffDeck.push(deck[p])
+            break;
+          } 
+        }
+      }
+    })
+    return shuffDeck
+  }
+
+  function dealCards(number, faceDown, position){
+    var dealtCards = []
+    var selectedCards = shuffledDeck.splice(0,number)
+    if (number > shuffledDeck.length) {
+      var notEnoughCards = shuffledDeck.splice(0,number)
+      notEnoughCards.map((card) => {
+        dealtCards.push({card: card, faceDown: faceDown, position: position})
+      })
+      dealtCards.push({card: "No More Cards"})
+    } else {
+    selectedCards.map((card) => {
+    dealtCards.push({card: card, faceDown: faceDown, position: position})
+    })
+    }
+    return dealtCards
+  }
+
+  function cutDeck(position) {
+    var deckCut = []
+    var deckCut = shuffledDeck.splice(position, 51)
+    deckCut.push(...shuffledDeck)
+    shuffledDeck = deckCut
+  }
 }
 
-function cutDeck(position) {
-  var deckCut = []
-  var deckCut = shuffledDeck.splice(position, 51)
-  deckCut.push(...shuffledDeck)
-  shuffledDeck = deckCut
-}
 /*
 document.createElement('body');
 var shuffle = document.createElement('button');
